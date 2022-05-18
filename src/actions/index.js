25b9ce1ad8855f8d4ad28675b21b9328470ae7c2
Victor Vidal <victorvidal08@hotmail.com>
2022-05-18 17:35:59 -0300

    adiciona req 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
 // Coloque aqui suas actions
+export const saveUserEmail = (email) => ({
+  type: 'SET_USER_EMAIL',
+  payload: email,
+});
+
+export const addCurrency = (currencies) => ({
+  type: 'ADD_CURRENCY',
+  payload: currencies,
+});
+
+export const addExpense = (expenses) => ({
+  type: 'ADD_EXPENSE',
+  payload: expenses,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,87 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveUserEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+  }
+
+  handleButton = () => {
+    // console.log(this.props);
+    const { history, setUserEmail } = this.props;
+    const { email } = this.state;
+    setUserEmail(email);
+    history.push('/carteira');
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, () => {
+      const { email, password } = this.state;
+      const passwordTest = 6;
+      const emailTest = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+      this.setState({
+        isDisabled: !(emailTest.test(email) && password.length >= passwordTest),
+      });
+      // validação com apoio do Takahashi
+    });
+  }
+  // ref regex: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    return (
+      <div>
+        Login
+        <form>
+          <label htmlFor="email">
+            Insira seu Email:
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              value={ email }
+            />
+          </label>
+          <label htmlFor="password">
+            Insira sua senha:
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ this.handleButton }
+            disabled={ isDisabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>);
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  setUserEmail: (email) => dispatch(saveUserEmail(email)),
+});
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.shape).isRequired,
+  setUserEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
