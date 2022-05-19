ed513db01c7a5fdd868af21a535aa3506ffda53d
Victor Vidal <victorvidal08@hotmail.com>
2022-05-18 21:26:05 -0300

    adiciona req 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -11,10 +11,63 @@ class Wallet extends React.Component {
   }
 
   render() {
+    const { currenciesArray } = this.props;
     return (
       <div>
         TrybeWallet
         <Header />
+        <form>
+          <label htmlFor="expense">
+            Despesa:
+            <input
+              type="text"
+              name="expense"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="description">
+            Descrição despesa:
+            <input
+              type="text"
+              name="description"
+              data-testid="description-input"
+            />
+          </label>
+          <label htmlFor="currency">
+            Moeda:
+            <select
+              name="currency"
+            >
+              { currenciesArray.map((currency, key) => (
+                <option key={ key }>{ currency }</option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento:
+            <select
+              name="payment"
+              data-testid="method-input"
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="category">
+            Categoria:
+            <select
+              name="category"
+              data-testid="tag-input"
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
@@ -24,8 +77,13 @@ const mapDispatchToProps = (dispatch) => ({
   setCurrencies: () => dispatch(fetchAPI()),
 });
 
+const mapStateToProps = (globalState) => ({
+  currenciesArray: globalState.wallet.currencies,
+});
+
 Wallet.propTypes = {
   setCurrencies: propTypes.func.isRequired,
+  currenciesArray: propTypes.arrayOf(propTypes.string).isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(Wallet);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
